b486d0f773a3e00f7cb157bab2529697431cf925
chooJoonbum <cjb3333@naver.com>
2024-03-14 17:49:53 +0900

    대시보드 테스트

---
+++ tests/Feature/Http/Controllers/Dashboard/CommentControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Dashboard;
+
+use App\Http\Middleware\RequirePassword;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testReturnsCommentsDashboardViewForListOfComment()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user)
+            ->withoutMiddleware(RequirePassword::class)
+            ->get(route('dashboard.comments'))
+            ->assertOk()
+            ->assertViewIs('dashboard.comments');
+    }
+
+}
