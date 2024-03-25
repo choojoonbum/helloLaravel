e50a12c8febae367b932cffdac95323f331183a1
chooJoonbum <cjb3333@naver.com>
2024-03-25 17:41:16 +0900

    배포파일 적용

---
+++ Envoy.blade.php
@@ -0,0 +1,59 @@
+@servers(['homestead' => 'vagrant@homestead'])
+
+@setup
+$repo = $repo = 'https://github.com/choojoonbum/helloLaravel.git';
+
+$releases = '~/releases';
+$app = '~/app';
+
+$shared = [
+'~/storage' => 'storage'
+];
+@endsetup
+
+@task('deploy', ['on' => ['homestead']])
+# Make 'releases' Directory
+mkdir -p {{ $releases }};
+
+# Git
+git clone -b {{ $tag }} {{ $repo }} {{ "$releases/$tag" }}
+cd {{ "$releases/$tag" }}
+
+# Create 'shared' symlinks
+@foreach ($shared as $global => $local)
+    if [ ! -d {{ $global }} ]; then
+    cp -r {{ $local }} {{ $global }};
+    fi;
+
+    [ -d {{ $local }} ] && rm -rf {{ $local }};
+    ln -nfs {{ $global }} {{ $local }};
+@endforeach
+
+# Install Dependencies
+composer install --optimize-autoloader --no-interaction --no-dev
+
+# Create Storage Links
+php artisan storage:link
+
+# Decrypt .env.production
+export LARAVEL_ENV_ENCRYPTION_KEY={{ $key }}
+export APP_ENV=production
+
+php artisan env:decrypt --env=production
+
+# Install Node.js Dependencies
+npm ci
+npm run build
+
+# Database Migration
+php artisan migrate --no-interaction --force
+
+# Optimize
+# @see https://laravel.com/docs/9.x/deployment#optimization
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Create a Symlink
+ln -nfs {{ "$releases/$tag" }} {{ $app }}
+@endtask
